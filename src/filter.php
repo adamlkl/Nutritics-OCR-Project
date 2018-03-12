677f68e4006ee208b8fd1df000c37fa0d46359df
Owen <duffyow@tcd.ie>
2018-03-12 00:23:33 +0000

    Added comments to provide boiler plate method

--- src/filter.php
+++ src/filter.php
@@ -1,5 +1,28 @@
 <?php
+/*
+	method
+	find 100g column
+		get its central x point
+	
+	search for serving size
+	
+	For every box in json
+		if collides with a search point
+			read text in box and store in relevant nutritional element.
+		else if description is one of our nutritional headings
+			get average y value of corners (middle of box)
+			use to find an intersection with x from 100g column
+			add intersection as a search point for that nutriotional value
+		
+*/
 
+/*
+	datastructures needed:
+		array of 7 nutritional headings (fat, energy etc)
+		corresponding array of 7 vertices where to expect the nutrition quantities
+		blank json response to fill with answer
+		
+*/
 function filterJSONresponse($response)
 {
 
