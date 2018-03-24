066ccf06cc3eec9c3d59e0d3b85b95b04d688b2a
adamlkl <kleong@tcd.ie>
2018-03-24 23:27:11 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,97 +1,33 @@
-<?php
-//require 'vendor/autoload.php';
-// # Includes the autoloader for libraries installed with composer
-require __DIR__ . '/../vendor/autoload.php';
-
-require_once(__DIR__ . '/google_ocr.php');
-
-require_once(__DIR__ . '/filter.php');
-
-header('Content-type: application/json');
-
-//print_r(getOCRResponseJSON(fopen('/app/tests/testdata/images/noodles.jpg', 'r')));
-
-$json_response = getOCRResponseJSON(fopen('/app/tests/testdata/images/noodles.jpg', 'r'));
-
-//print_r($json_response);
-
-
-
-
-$response = json_decode($json_response);
-
-$final = filterJSONresponse($response);
-
-print_r($final);
-
-/*
-$key = array_search('Fat', $response);
-
-echo($key);
-
-*/
-
-
-// check for consistency in output
-/*
-foreach($response->textAnnotations as $text)
-{
-	//echo($text->description);
-
-	if ($text->description == "Fat")
-	{
-	echo($text->description);
-
-	}
-
-}
-
-*/
-//$result = array_filter($response['description'], function($fat))
-//{
-
-
-//return $fat
-//}
-
-/*
-foreach($response['responses'][0]['textAnnotations'] as $box)
-{
-
-				$points = array();
-
-				foreach($box['boundingPoly']['vertices'] as $vertex)
-				{
-					array_push($points, $vertex['x'], $vertex['y']);
-				}
-
-				imagepolygon($im, $points, count($box['boundingPoly']['vertices']), $red);
-
-}
-*/
-
-
-
-
-//print_r($json_response);
-//$response = json_decode($json_response, true);
-//print_r($json_response);
-
-
-//$Res = json_decode(getOCRResponseJSON(fopen('/app/tests/testdata/images/noodles.jpg', 'r')));
-
-
-
-
-
-//$filterRes = json_decode($obj);
-//print($filterRes);
-
-//$JSONresponse = getOCRResponseJSON(fopen('/app/tests/testdata/images/noodles.jpg', 'r'));
-//$obj = json_decode($JSONresponse);
-
-//print $obj->{'Nutrition'};
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+    <title></title>
+    <style type = "text/css">
+    	body {
+    		font-family : Georgia,Times,serif;
+    	}
+    	h1 {
+    		font-family:  "Courier New", Courier, monospace;
+    		font-size: 16px;
+    		padding-top: 20px;
+    		padding-left: 480px;
+    	}
+    </style>
+</head>
+<body>
+	<h1>Nutritics OCR API Demo</h1>
+	<div class="grid">
+		<div>
+            <?php require_once(__DIR__ . '/NutriticsOCR.php'); ?>
+            <?php $result = new NutriticsOCR($POST)->getResult(); ?>
+			<form method="POST" enctype="multipart/form-data" name="uploadForm">
+    		<input type="file" name="file">
+    		<button type="submit" name="submit" style="font-size: 12px; font-family: cambria; height: 18px; width:70px;">UPLOAD</button></div>
+		<div style="font-family: Ariel,Courier,serif; font-size: 18px;">Result:<br/><?php print_r($result); ?></div>
+	</div>
+</form>
+</body>
+</html>
